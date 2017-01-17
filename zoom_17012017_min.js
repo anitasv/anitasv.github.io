2e475dd9bcc91f10817ea1b8e7f578fa3110169b
Anita SV <anitvasu@amazon.com>
2017-01-17 16:22:02 +0530

    Unminify

--- zoom_17012017_min.js
+++ zoom_17012017_min.js
@@ -1,4 +1,320 @@
-function g(a,b){return[a*b[0],a*b[1]]}function k(a,b){return[a[0]+b[0],a[1]+b[1]]}function l(a,b){return[a[0]-b[0],a[1]-b[1]]}function m(a,b){return k(g(b[0],a[0]),g(b[1],a[1]))}function n(a,b){this.b=a;this.a=b}function p(a){var b=a.b;a=a.a;return"matrix("+b[0][0]+","+b[0][1]+","+b[1][0]+","+b[1][1]+","+a[0]+","+a[1]+")"}function q(a,b){var c=a.b,d=b.b;return new n([m(c,d[0]),m(c,d[1])],k(m(a.b,b.a),a.a))}var u=new n([[1,0],[0,1]],[0,0]);
-function v(a){function b(b){var c=a.offsetLeft,d=a.offsetTop;return[[b[0].pageX-c,b[0].pageY-d],[b[1].pageX-c,b[1].pageY-d]]}this.c=a;this.b=!1;this.a=u;this.f=null;this.h=[0,0];var c=this,d=!1;a.style["transform-origin"]="0 0";a.parentNode.addEventListener("touchstart",function(a){var r=a.touches;if(!r)return!1;a.preventDefault();2===r.length?(c.h=b(r),c.b=!0):1==r.length&&(d?(d=!1,c.g()):d=setTimeout(function(){d=!1},300))});a.parentNode.addEventListener("touchmove",function(a){var d=a.touches;
-if(!d||2!=d.length||!c.b)return!1;a.preventDefault();a=c.h;var d=b(d),h,e=l(a[1],a[0]),t=l(d[1],d[0]),w=e[0]*e[0]+e[1]*e[1];h=(e[0]*t[0]+e[1]*t[1])/w;e=(e[0]*t[1]-e[1]*t[0])/w;h=[[h,e],[-e,h]];c.f=new n(h,l(d[0],m(h,a[0])));c.c.style.transform=p(q(c.f,c.a))});a.parentNode.addEventListener("touchend",function(){c.b&&(c.a=q(c.f,c.a),c.b=!1)})}
-v.prototype.g=function(){if(window.requestAnimationFrame){var a=this.a,b=null,c=this,d=function(f){b||(b=f);f=(f-b)/100;1<f?(c.a=u,c.b=!1,c.c.style.transform=p(c.a)):(c.c.style.transform=p(new n([k(g(1-f,A[0]),g(f,B[0])),k(g(1-f,A[1]),g(f,B[1]))],k(g(1-f,a.a),g(f,u.a)))),window.requestAnimationFrame(d))};window.requestAnimationFrame(d)}else this.a=u,this.b=!1,this.c.style.transform=p(this.a)};v.prototype.reset=v.prototype.g;window.Zoom=v;
+// Type Matrix is [ [a00, a10], [a01, a11] ]
+// Type Vector is [ x, y ]
+// Type Transform is [ Matrix, Vector ]
+
+/**
+ * Multiply Scalar with Vector returns a Vector.
+ * 
+ * @param {number} l scalar to multiply with
+ * @param {Array<number>} x 2D vector.
+ * @return {Array<number>}
+ */
+var scmult = function(l, x) {
+    return [ l * x[0], l * x[1] ];
+};
+
+/**
+ * Adding two vectors is another vector.
+ * 
+ * @param {Array<number>} a 2D vector.
+ * @param {Array<number>} b 2D vector.
+ * @return {Array<number>} Sum vector.
+ */
+var vcadd = function(a, b) {
+    return [ a[0] + b[0], a[1] + b[1] ];
+};
+
+/**
+ * Subtracting two vectors is another vector.
+ * 
+ * @param {Array<number>} a 2D vector.
+ * @param {Array<number>} b 2D vector.
+ * @return {Array<number>} Difference vector.
+ */
+var minus = function(a, b) {
+    return [ a[0] - b[0], a[1] - b[1] ];
+};
+
+/**
+ * Dot product of two vectors is scalar.
+ * 
+ * @param {Array<number>} a 2D vector.
+ * @param {Array<number>} b 2D vector.
+ * @return {number} scalar inner product.
+ */
+var dot = function(a, b) {
+    return a[0] * b[0] + a[1] * b[1];
+};
+
+/**
+ * Exterior Product of two vectors is a pseudoscalar.
+ * 
+ * @param {Array<number>} a 2D vector.
+ * @param {Array<number>} b 2D vector.
+ * @return {number} psuedo-scalar exterior product.
+ */
+var wedge = function(a, b) {
+    return a[0] * b[1] - a[1] * b[0];
+};
+
+/**
+ * Apply Matrix on Vector returns a Vector.
+ * 
+ * @param {Array<Array<number>>} A 2x2 Matrix
+ * @param {Array<number>} x 2D vector.
+ * @return {Array<number>} 2D vector linear product.
+ */
+var apply = function(A, x) {
+    return vcadd(scmult(x[0], A[0]), scmult(x[1], A[1]));
+};
+
+/**
+ * Multiply two matrices.
+ * 
+ * @param {Array<Array<number>>} A 2x2 Matrix
+ * @param {Array<Array<number>>} B 2x2 Matrix
+ * @return {Array<Array<number>>} A 2x2 Matrix
+ */
+var mult = function(A, B) {
+    return [ apply(A, B[0]), apply(A, B[1]) ];
+};
+
+/**
+ * Represents a transform operation, Ax + b
+ * 
+ * @constructor
+ * 
+ * @param {Array<Array<number>>} A 2x2 Matrix.
+ * @param {Array<number>} b 2D scalar.
+ */
+function Transform(A, b) {
+    this.A = A;
+    this.b = b;
+}
+
+/**
+ * Given CSS Transform representation of the class.
+ * @return {string} CSS 2D Transform. 
+ */
+Transform.prototype.css = function() {
+    var A = this.A;
+    var b = this.b;
+    return 'matrix(' + A[0][0] + ',' + A[0][1] + ',' + A[1][0] + ',' + A[1][1] +
+            ',' + b[0] + ',' + b[1] + ')';
+};
+
+/**
+ * Multiply two transforms. 
+ * Defined as 
+ *  (T o U) (x) = T(U(x))
+ * 
+ * Derivation:
+ *  T(U(x)) 
+ *   = T(U.A(x) + U.b) 
+ *   = T.A(U.A(x) + U.b)) + T.b
+ *   = T.A(U.A(x)) + T.A(U.b) + T.b 
+ * 
+ * @param {Transform} T 
+ * @param {Transform} U 
+ * @return {Transform} T o U
+ */
+var cascade = function(T, U) {
+    return new Transform(mult(T.A, U.A), vcadd(apply(T.A, U.b), T.b));
+};
+
+/**
+ * Creates the default rotation matrix
+ * 
+ * @param {number} c x-projection (r cos(theta))
+ * @param {number} s y-projection (r sin(theta))
+ * @return {Array<Array<number>>} Rotation matrix.
+ */
+var rotate = function(c, s) {
+    return [ [ c, s], [-s, c] ];
+};
+
+/**
+ * Returns matrix that transforms vector a to vector b.
+ * 
+ * @param {Array<number>} a 2D vector.
+ * @param {Array<number>} b 2D vector.
+ * @return {Array<Array<number>>} Rotation + Scale matrix
+ */
+var rotscale = function(a, b) {
+    var alen = dot(a, a);
+    var sig = dot(a, b);
+    var del = wedge(a, b);
+    return rotate( sig / alen, del / alen);
+};
+
+/**
+ * Zoom is a similarity preserving transform from a pair of source
+ * points to a new pair of destination points.
+ * 
+ * @param {Array<Array<number>>} s two source points.
+ * @param {Array<Array<number>>} d two destination points.
+ * 
+ * @return {Transform} that moves point 's' to point 'd' 
+ */ 
+var zoom = function(s, d) {
+    // Source vector.
+    var a = minus(s[1], s[0]);
+    // Destination vector.
+    var b = minus(d[1], d[0]);
+    // Rotation needed for source to dest vector.
+    var rs = rotscale(a, b);
+
+    // Position of s[0] if rotation is applied.
+    var rs0 = apply(rs, s[0]);
+    // Since d[0] = rs0 + t
+    var t = minus(d[0], rs0);
+
+    return new Transform(rs, t);
+};
+
+/**
+ * Weighted average of two vectors.
+ * 
+ * @param {Array<number>} u 2D vector.
+ * @param {Array<number>} v 2D vector.
+ * @param {number} progress (from 0 to 1)
+ * @return {Array<number>} (1-p) u + (p) v 
+ */
+var avgVector = function(u, v, progress) {
+    var u1 = scmult(1 - progress, u);
+    var v1 = scmult(progress, v);
+    return vcadd(u1, v1);
+};
+
+/**
+ * Weighted average of two vectors.
+ * 
+ * @return {Array<Array<number>>} A 2D matrix.
+ * @return {Array<Array<number>>} B 2D matrix.
+ * @param {number} progress (from 0 to 1)
+ * @return {Array<Array<number>>} (1-p) A + (p) B 
+ */
+var avgMatrix = function(u, v, progress) {
+    return [ avgVector(A[0], B[0], progress),  avgVector(A[1], B[1], progress) ];
+};
+
+
+/**
+ * Weighted average of two transforms.
+ * @param {Transform} Z Source Transform
+ * @param {Transform} I Destination Transform
+ * @param {number} progress (from 0 to 1)
+ * @return {Transform} (1-p) Z + (p) I 
+ */
+Transform.avg = function(Z, I, progress) {
+    return new Transform(avgMatrix(Z.A, I.A, progress), avgVector(Z.b, I.b, progress));
+};
+
+var identity = new Transform([[1, 0], [0, 1]], [0, 0]);
+
+/**
+ * @constructor
+ * @export
+ * @param {Element} elem to attach zoom handler.
+ */
+function Zoom(elem) {
+    this.elem = elem;
+    this.zooming = false;
+    this.activeZoom = identity;
+    this.currentZoom = null;
+    this.srcCoords = [0, 0];
+
+    var me = this;
+    var tapped = false;
+
+    elem.style['transform-origin'] = '0 0';
+
+    var getCoords = function(t) {
+        var oX = elem.offsetLeft;
+        var oY = elem.offsetTop; 
+        return [ 
+            [t[0].pageX - oX, t[0].pageY - oY],
+            [t[1].pageX - oX, t[1].pageY - oY] 
+        ];
+    };
+
+    elem.parentNode.addEventListener('touchstart', function(evt) {
+        var t = evt.touches;
+        if (!t) {
+            return false;
+        }
+        evt.preventDefault();
+        if (t.length === 2) {
+            me.srcCoords = getCoords(t);
+            me.zooming = true;
+        } else if (t.length == 1) {
+            if (!tapped) {
+                tapped = setTimeout(function() {
+                    tapped = false;
+                }, 300);
+            } else {
+                tapped = false;
+                me.reset();
+            }
+        }
+    });
+    
+    elem.parentNode.addEventListener('touchmove', function(evt) {
+        var t = evt.touches;
+        if (!t || t.length != 2) {
+            return false;
+        }
+        if (!me.zooming) {
+            // To prevent possible potential reorder of events.
+            return false;
+        }
+        evt.preventDefault();
+        var destCoords = getCoords(t);
+        me.currentZoom = zoom(me.srcCoords, destCoords);
+        var finalT = cascade(me.currentZoom, me.activeZoom);
+        me.update(finalT);
+    });
+
+    elem.parentNode.addEventListener('touchend', function() {
+        if (me.zooming) {
+            me.activeZoom = cascade(me.currentZoom, me.activeZoom);
+            me.zooming = false;
+        }
+    });
+}
+
+Zoom.prototype.update = function(finalT) {
+    this.elem.style.transform = finalT.css();
+};
+
+Zoom.prototype.reset = function() {
+    if (window.requestAnimationFrame) {
+        var Z = this.activeZoom;
+        var startTime = null;
+
+        var me = this;
+
+        var step = function(time) {
+            if (!startTime) { 
+                startTime =  time;
+            }
+            var progress = (time - startTime)/100;
+            if (progress > 1) {
+                me.activeZoom = identity;
+                me.zooming = false;
+                me.update(me.activeZoom);
+            } else {
+                me.update(Transform.avg(Z, identity, progress));
+                window.requestAnimationFrame(step);
+            }
+        };
+        window.requestAnimationFrame(step);
+    } else {
+        this.activeZoom = identity;
+        this.zooming = false;
+        this.update(this.activeZoom);
+    }
+};
+Zoom.prototype['reset'] = Zoom.prototype.reset;
+
+window['Zoom'] = Zoom;
